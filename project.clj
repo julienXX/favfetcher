d859fd682fdba91284f41faafe7036cdd48c251b
Julien Blanchard <julien@sideburns.eu>
2012-04-10 01:15:24 +0200

    check for url existence

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
             :description "Favicon fetcher"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]
-                           [enlive "1.0.0-SNAPSHOT"]]
+                           [enlive "1.0.0-SNAPSHOT"]
+                           [clj-http "0.3.6"]]
             :main favfetcher.server)
 
--- src/favfetcher/views/root.clj
+++ src/favfetcher/views/root.clj
@@ -2,7 +2,8 @@
   (:require [favfetcher.views.common :as common]
             [noir.content.getting-started]
             [noir.response :as response]
-            [net.cgrand.enlive-html :as html])
+            [net.cgrand.enlive-html :as html]
+            [clj-http.client :as client])
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]))
 
@@ -12,5 +13,13 @@
 
 (defpage [:post "/"] {:keys [url]}
          (if url
-           (response/json {:url url})
-           (response/json {:error "You must have a url parameter in your request"})))
+           (if (= (val (find (client/get url {:throw-exceptions false}) :status)) 200)
+             (response/json {:status 200 :message url})
+             (response/json {:status 404 :message "Unreachable url"}))
+           (response/json {:status 500 :message "You must have a url parameter in your request"})))
+
+(defn fetch-url [url]
+      (html/html-resource (java.net.URL. url)))
+
+(defn favicon-at-root-domain [url]
+      (client/get url))
